b88171dea6373218d70a631cb0a490576f1f1109
Kanem <kanemdeng@email.com>
2022-07-23 18:08:48 -0700

    add delete button by using methodOVerride

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 require('dotenv').config();
+const  methodOverride = require('method-override')
 const app = express()
 const PORT = process.env.PORT || 3000
 
@@ -10,13 +11,14 @@ const articlesRouter = require('./server/routes/articles.js')
 
 
 
+
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended:true}))
 
+app.use(methodOverride('_method'))
+
 app.use('/markdown', markdownRouter)
 app.use('/markdown/articles', articlesRouter)
-
-
 
 app.listen(PORT, () => {
   console.log(`Listening port: ${PORT}`)
--- server/routes/articles.js
+++ server/routes/articles.js
@@ -2,14 +2,12 @@ const express = require('express')
 const router = express.Router()
 const articles = require('../controllers/articlesController.js')
 
-
-
-router.get('/', articles.allArticles)
-router.get('/new',articles.newPage)
-router.post('/', articles.postNewArticle)
-router.get('/:slug', articles.articleId)
 router.delete('/:id',articles.deleteId)
 
+router.get('/new',articles.newPage)
+router.get('/:slug', articles.articleId)
+router.get('/', articles.allArticles)
 
+router.post('/', articles.postNewArticle)
 
 module.exports = router
